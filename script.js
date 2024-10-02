48ba9b09dde0bcb7d8f56d70ddedb41931d9f6f0
Your Name <zohaibabbas8557@gmail.com>
2024-10-02 21:24:14 +0500

    added the fucntionality to button

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-let inputDate = document.querySelector(".date").value;
+ document.querySelector(".cal-btn").addEventListener("click",()=>{
+    let inputDate = document.querySelector(".date").value;
+if(! inputDate){
+    alert("please! Enter the date?")
+}
+ });
 let dateSelected = new Date(inputDate);
   let todayDate =  new Date();
 
@@ -7,4 +12,5 @@ let dateSelected = new Date(inputDate);
   let years = todayDate.getFullYear() - dateSelected.getFullYear();
   let months = todayDate.getMonth() - dateSelected.getMonth();
   let days = todayDate.getDate() - dateSelected.getDate();
-  console.log(years,days,months)
+
+
