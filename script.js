8779671ca225301031a05a0c2702577c0aff386c
Your Name <zohaibabbas8557@gmail.com>
2024-10-02 21:44:03 +0500

    all upto dated

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
- document.querySelector(".cal-btn").addEventListener("click",()=>{
-    let inputDate = document.querySelector(".date").value;
-if(! inputDate){
-    alert("please! Enter the date?")
-}
- });
-let dateSelected = new Date(inputDate);
-  let todayDate =  new Date();
+document.querySelector('.cal-btn').addEventListener('click', function() {
+    // Get the input date value
+    let inputDate = document.querySelector('.date').value;
+    if (!inputDate) {
+        alert("Please! Enter the date.");
+        return;
+    }
 
+    const selectedDate = new Date(inputDate);
+    const currentDate = new Date();
 
+    let years = currentDate.getFullYear() - selectedDate.getFullYear();
+    let months = currentDate.getMonth() - selectedDate.getMonth();
+    let days = currentDate.getDate() - selectedDate.getDate();
 
-  let years = todayDate.getFullYear() - dateSelected.getFullYear();
-  let months = todayDate.getMonth() - dateSelected.getMonth();
-  let days = todayDate.getDate() - dateSelected.getDate();
+    if (days < 0) {
+        months--;
+        days += new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate();
+    }
+
+    if (months < 0) {
+        years--;
+        months += 12;
+    }
+
+    document.querySelector('.year .data').innerHTML = `${years}<br><span>years</span>`;
+    document.querySelector('.month .data').innerHTML = `${months}<br><span>months</span>`;
+    document.querySelector('.days .data').innerHTML = `${days}<br><span>days</span>`;
+});
 
 
